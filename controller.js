c2ed5ea056dedc4d1d6776043c32bbeaf793504c
FreddyCalk <freddycalk@gmail.com>
2015-11-13 10:33:42 -0500

    adding comments

--- controller.js
+++ controller.js
@@ -1,41 +1,44 @@
 // Angular App Module and controller
 angular.module('myApp',[]).controller('mapController',function($scope){
 
-
+	// Initializing the map options on load
 	var mapOption = {
 		zoom: 4,
 		center: new google.maps.LatLng(40.000,-98.000),
 		mapTypeId: google.maps.mapTypeId
 	}
-
+	// makes the new google maps object visible to the DOM via Angular
 	$scope.map = new google.maps.Map($('#map')[0],mapOption)
 	$scope.markers = [];
-
+	// adding an info window to each marker that will appear on hover.
 	var infoWindow = new google.maps.InfoWindow();
-
-
-
+	// This function serves to place the specified marker on the city with which it correlates.
 	function createMarker(city,index){
 
 		if(index==0){
+			// New York City Icon
         	icon = 'media/1.png';
         }else if(index==38){
+        	// Atlanta, GA Icon
         	icon ='media/atl.png';
         }else{
+        	// default Icon image for the rest of the cities
         	icon = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2%7CFE7569';
         }
-
+        // method to get information from the JavaScript object into a useable format for the Google API.
 		var latLon = city.latLon.split(' ');
+		// finds the location of the degree symbol and slices the string from the beginning to the character
+		// right before the degree symbol.
 		var lat = latLon[0].slice(0,latLon[0].indexOf('&#176')-1);
-		var lon = -latLon[1].slice(0,latLon[1].indexOf('&#176')-1);
-
+		var lon = '-'+latLon[1].slice(0,latLon[1].indexOf('&#176')-1);
+		// Defines the marker options
 		var marker = new google.maps.Marker({
 			map: $scope.map,
 			position: new google.maps.LatLng(lat, lon),
 			title: city.city,
 			icon: icon
 		})
-
+		// defining the HTML content of the infor boxes that appear when you click on a marker.
 		var markerContentHTML = '<div class="infoWindowContent">';
 		    markerContentHTML += '<div class="total-pop">Total Population: ' + city.yearEstimate + '</div>';
 		    markerContentHTML += '<div class="pop-dens-last-year">2010 Census: ' + city.lastCensus + '</div>';
@@ -46,20 +49,21 @@ angular.module('myApp',[]).controller('mapController',function($scope){
 		    markerContentHTML += '<a href="#" onclick="getDirections('+lat+','+lon+')">Get directions</a><br>';
 		    markerContentHTML += '<a href="#" onclick="lodgingSearch('+lat+','+lon+')">Get Lodging</a><br>';
 		    markerContentHTML += '<a href="#" onclick="grocerySearch('+lat+','+lon+')">Find Food</a>';
-
 	    	markerContentHTML += '</div>';
     		marker.content = markerContentHTML;
 	    	google.maps.event.addListener(marker, 'click', function(){
 	    	infoWindow.setContent('<h2>'+ marker.title + '</h2>' + marker.content);
 	    	infoWindow.open($scope.map, marker);
 	    });
-
+    	// adding the marker that just got made to the total marker Array.
     	$scope.markers.push(marker)
 	}
+	// function that binds the buttons to the info boxes for each city
 	$scope.triggerClick = function(i){
 		google.maps.event.trigger($scope.markers[i-1],'click');
 	}
-
+	// when the search bar is submitted, the map markers are updated to show only the cities
+	// that appear in the search results
 	$scope.updateMarkers = function(){
 		for(i=0;i<$scope.markers.length;i++){
 			$scope.markers[i].setMap(null);
@@ -67,9 +71,9 @@ angular.module('myApp',[]).controller('mapController',function($scope){
 		for(i=0;i<$scope.filteredCities.length;i++){
 			createMarker($scope.filteredCities[i],i);
 		}
+		// clears the input field on submit
 		$('#filter-input').val('')	
 	}
-
 
 	getDirections = function(lat,lon){
 		var directionsService = new google.maps.DirectionsService();
@@ -79,16 +83,16 @@ angular.module('myApp',[]).controller('mapController',function($scope){
    			mapTypeId: google.maps.MapTypeId.ROADMAP
    		})
    		directionsDisplay.setMap(map);
-   		directionsDisplay.setPanel($('#map-panel')[0]);
+   		directionsDisplay.setPanel($('#map-panel')[0]);\
+   		// Prompt the user to input their current location.
    		var origin = prompt('Enter your current location!');
    		var request = {
-           //Origin hardcoded to Atlanta. Require geocode current loc,
-           //or give user input
           origin: origin, 
-          destination: new google.maps.LatLng(lat,lon), 
+          destination: new google.maps.LatLng(lat,lon),
+          // specifies that we want driving directions 
           travelMode: google.maps.DirectionsTravelMode.DRIVING
         };
-
+        // calls directions service so that the route can be plotted on the map
         directionsService.route(request, function(response, status) {
           if (status == google.maps.DirectionsStatus.OK) {
             directionsDisplay.setDirections(response);
